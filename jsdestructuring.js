c1a5a2017ae1b064c8daf50169c0b5494c14960d
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-15 19:40:04 +0000

    16

---
+++ jsdestructuring.js
@@ -0,0 +1,22 @@
+//this used to destructure elemtns from arrays or objects for easier extraction
+
+
+// let students={
+//     student1:"kishore",
+//     student2:"junaid"
+// }
+
+// console.log(students.student1); //old fashined way of accessing key value 
+
+
+
+//new way introduced by es6 to access called js destructuring
+// let students={
+//     student1:"kishore",
+//     student2:"junaid"
+// }
+
+// let {student1,student2}=students
+// console.log(student1);
+
+// console.log(student2); 
