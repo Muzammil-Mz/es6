0594c798b21e56bdcea63a0592d93fefd406e124
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-15 19:53:19 +0000

    16

---
+++ defaultparam2.js
@@ -0,0 +1,11 @@
+// function wihses(hey="hello"){ //using default para
+//     console.log(hey,"kk");
+    
+// }
+// wihses()// hello kk as output when no param passed
+
+// function wihses(hey="hello"){ //using default para
+//     console.log(hey,"kk");
+    
+// }
+// wihses("im good")// im good kk as value passed
---
+++ importExport.js
@@ -0,0 +1,7 @@
+//import export js helps to import anything from one file to another and export from one file to anotheer 
+
+// export default function greet(name){
+//     console.log(`hi ${name}`);
+    
+// }
+
