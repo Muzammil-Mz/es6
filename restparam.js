8d458969c18734d8f71b953ead83f9fd64c0946b
Muzammil-Mz <Mujjuimran74@gmail.com>
2024-11-15 19:59:11 +0000

    16

---
+++ restparam.js
@@ -0,0 +1,11 @@
+// JavaScript Rest Parameter
+// You can use the rest parameter ... to represent an infinite number of arguments as an array. For example,
+
+// function show(a,b,c,d){
+// console.log("a:",a);
+// console.log("b:",b);
+// console.log("c:",c);
+// console.log("d:",d);
+
+// }
+// show ("apple","ball","cat",55)
